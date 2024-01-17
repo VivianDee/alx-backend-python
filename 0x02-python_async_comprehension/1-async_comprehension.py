267f4b0da27c7d258ce01502379cceb90742f643
VivianDee <vdagbue@gmail.com>
2024-01-16 20:15:03 -0800

    [fix]: return type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> List[float]:
     """Collect 10 random numbers"""
     numbers = [i async for i in async_generator()]
     return numbers
