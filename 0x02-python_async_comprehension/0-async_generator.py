dbb5acc3829d60b059855c0a669f8a1612e6b054
VivianDee <vdagbue@gmail.com>
2024-01-16 20:09:01 -0800

    [fix]: return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,11 +4,11 @@
 
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[int]:
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine will loop 10 times"""
     for i in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
-        await asyncio.sleep(1.0)
